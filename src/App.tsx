928d681ecbfc1817e52b9156221d616ac063c712
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-10-25 22:56:18 +0630

    Event props typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Status } from './components/Status';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+// import { Greet } from './components/Greet';
+// import { Heading } from './components/Heading';
+// import { Oscar } from './components/Oscar';
+// import { Status } from './components/Status';
 // import { Greet } from './components/Greet';
 // import { Person } from './components/Person';
 // import { PersonList } from './components/PersonList';
@@ -34,12 +36,16 @@ function App() {
       {/* <Greet name="Vishwas" messageCount={20} isLoggedIn={true}/>
       <Person name={personName} />
       <PersonList names={nameList} /> */}
-      <Status status='loading' />
+      {/* <Status status='loading' />
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading>Oscar goes to Loonardo Dicpario!</Heading>
       </Oscar>
-      <Greet name="Vishwas" isLoggedIn={true}/>
+      <Greet name="Vishwas" isLoggedIn={true}/> */}
+      <Button handleClick={(event , id ) => {
+        console.log("Button clicked", event , id)
+      }} />
+      <Input value='' handleChange={(event)=> console.log(event)} />
     </div>
   );
 }
