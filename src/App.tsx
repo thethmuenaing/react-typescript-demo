63606683084035d264163d4d4e63e26c16967297
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-10-26 10:10:06 +0630

    multiple use of props

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
 import './App.css';
-import { Container } from './components/Container';
+// import { Container } from './components/Container';
 // import { Button } from './components/Button';
-import { Input } from './components/Input';
+// import { Input } from './components/Input';
 // import { Greet } from './components/Greet';
 // import { Heading } from './components/Heading';
 // import { Oscar } from './components/Oscar';
 // import { Status } from './components/Status';
 // import { Greet } from './components/Greet';
-// import { Person } from './components/Person';
-// import { PersonList } from './components/PersonList';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
-  // const personName = {
-  //   first: "Bruce",
-  //   last: "Wayne",
-  // }
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  }
 
-  // const nameList =[
-  //   {
-  //     first:"Bruce",
-  //     last: "Wayne",
-  //   },
-  //   {
-  //     first:"Clark",
-  //     last: "Kent",
-  //   },
-  //   {
-  //     first:"Princess",
-  //     last: "Diana",
-  //   },
+  const nameList =[
+    {
+      first:"Bruce",
+      last: "Wayne",
+    },
+    {
+      first:"Clark",
+      last: "Kent",
+    },
+    {
+      first:"Princess",
+      last: "Diana",
+    },
 
-  // ]
+  ]
 
   return (
     <div className="App">
       {/* <Greet name="Vishwas" messageCount={20} isLoggedIn={true}/> */}
-      {/* <Person name={personName} /> */}
-      {/* <PersonList names={nameList} /> */}
+      <Person name={personName} />
+      <PersonList names={nameList} />
       {/* <Status status='loading' /> */}
       {/* <Heading>Placeholder text</Heading> */}
       {/* <Oscar>
@@ -46,8 +46,8 @@ function App() {
       {/* <Button handleClick={(event , id ) => {
         console.log("Button clicked", event , id)
       }} /> */}
-      <Input value='' handleChange={(event)=> console.log(event)} />
-      <Container styles={{border: "1px solid black", padding: "1rem"}} />
+      {/* <Input value='' handleChange={(event)=> console.log(event)} /> */}
+      {/* <Container styles={{border: "1px solid black", padding: "1rem"}} /> */}
     </div>
   );
 }
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,10 +1,4 @@
-type PersonProps = {
-	name: {
-		first:string
-		last:string
-	}
-}
-
+import { PersonProps } from "./Person.types"
 
 export const Person = (props: PersonProps) => {
 	return (
