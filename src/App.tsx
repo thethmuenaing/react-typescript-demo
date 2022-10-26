464dfc0ea51399f8d9ec3423943166ab071dfcbf
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-10-26 09:50:23 +0630

    destructure of typescript props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { Container } from './components/Container';
 // import { Button } from './components/Button';
-// import { Input } from './components/Input';
+import { Input } from './components/Input';
 // import { Greet } from './components/Greet';
 // import { Heading } from './components/Heading';
 // import { Oscar } from './components/Oscar';
@@ -34,19 +34,19 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Greet name="Vishwas" messageCount={20} isLoggedIn={true}/>
-      <Person name={personName} />
-      <PersonList names={nameList} /> */}
-      {/* <Status status='loading' />
-      <Heading>Placeholder text</Heading>
-      <Oscar>
+      {/* <Greet name="Vishwas" messageCount={20} isLoggedIn={true}/> */}
+      {/* <Person name={personName} /> */}
+      {/* <PersonList names={nameList} /> */}
+      {/* <Status status='loading' /> */}
+      {/* <Heading>Placeholder text</Heading> */}
+      {/* <Oscar>
         <Heading>Oscar goes to Loonardo Dicpario!</Heading>
-      </Oscar>
-      <Greet name="Vishwas" isLoggedIn={true}/> */}
+      </Oscar> */}
+      {/* <Greet name="Vishwas" isLoggedIn={true}/> */}
       {/* <Button handleClick={(event , id ) => {
         console.log("Button clicked", event , id)
-      }} />
-      <Input value='' handleChange={(event)=> console.log(event)} /> */}
+      }} /> */}
+      <Input value='' handleChange={(event)=> console.log(event)} />
       <Container styles={{border: "1px solid black", padding: "1rem"}} />
     </div>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,11 +6,11 @@ type InputProps = {
 }
 
 
-export const Input = (props: InputProps) => {
+export const Input = ({value , handleChange}: InputProps) => {
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		console.log(event)
 	}
 	return (
-		<input type="text" value={props.value} onChange={handleInputChange} />
+		<input type="text" value={value} onChange={handleChange} />
 	)
 }
