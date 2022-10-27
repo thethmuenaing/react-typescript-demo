3bc655b8fce8f99debb402c3dc5c2434bbb7eb9e
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-10-27 21:58:48 +0630

    Restricting Props typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { List } from './components/generics/List';
+import { RandomNumber } from './components/restriction/RandomNumber';
+// import { List } from './components/generics/List';
 // import { Private } from './components/auth/Private';
 // import { Profile } from './components/auth/Profile';
 // import { Counter } from './components/class/Counter';
@@ -75,7 +76,7 @@ function App() {
         items={[1, 2, 3]} 
         onClick={(item) => console.log("item ",item)}
       /> */}
-      <List 
+      {/* <List 
         items = {[
           {
             id: 1,
@@ -94,7 +95,8 @@ function App() {
           }
         ]}
         onClick={(item) => console.log("item ",item)}
-      />
+      /> */}
+      <RandomNumber value={10} isPositive/>
     </div>
   );
 }
---
+++ src/components/restriction/RandomNumber.tsx
@@ -0,0 +1,35 @@
+type RandomNumberType = {
+	value: number
+}
+type PositiveNumber = RandomNumberType & {
+	isPositive: boolean
+	isNegative?: never
+	isZero?: never
+}
+type NegativeNumber = RandomNumberType & {
+	isNegative: boolean
+	isPositive?: never
+	isZero?: never
+}
+type ZeroNumber = RandomNumberType & {
+	isZero: boolean
+	isPositive?: never
+	isNegative?: never
+}
+
+
+type RandomNumberProps = PositiveNumber | NegativeNumber | ZeroNumber
+
+export const RandomNumber = ({
+	value,
+	isPositive,
+	isNegative,
+	isZero,
+}: RandomNumberProps) => {
+	return (
+		<div>
+			{value} {isPositive && "Positive"} {isNegative && " Negative"} 
+			{isZero && " zero"}
+		</div>
+	)
+}
